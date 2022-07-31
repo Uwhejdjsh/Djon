cadc492f0b83e2688347266ab945603f7a6fa080
owjrnfks <81434949+Uwhejdjsh@users.noreply.github.com>
2022-07-31 23:12:18 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 import os
-if __name__ == "__main__":
-   try:
-       os.system("git pull")
-       import("jk").menu()
-   except Exception as e:
-       exit(str(e))
+os.system('git pull')
+import jk
+Main()
