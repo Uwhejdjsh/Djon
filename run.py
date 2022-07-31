bdeb146e2354a7923ff22ba42d062d5de08ac2e7
owjrnfks <81434949+Uwhejdjsh@users.noreply.github.com>
2022-07-31 22:59:53 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("mala").menu()
+       __import__("jk").menu()
    except Exception as e:
        exit(str(e))
